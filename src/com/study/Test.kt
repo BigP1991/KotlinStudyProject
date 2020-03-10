dd58383536198c6e7c2b4b2f3c63126bdf8d9a0a
K0100062 <K0100062@test.htsc.com.cn>
2020-03-10 15:55:14 +0800

    init

---
+++ src/com/study/Test.kt
@@ -0,0 +1,23 @@
+package com.study
+
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
+/**
+ * Created by zhanghaochen on 2020/3/10.
+ */
+fun main(args: Array<String>) {
+    // 内存中创建一个宽高为100的图片
+    var image = BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB)
+    var width = 0..99
+    var height = 0..99
+    image.apply {
+        for (i in width) {
+            for (j in height) {
+                setRGB(i, j, 0xff0000)
+            }
+        }
+    }
+    ImageIO.write(image, "bmp", File("a.bmp"))
+}
